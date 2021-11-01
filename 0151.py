c198ce7ec0f17ce830a0ba3a04c73b4b24b6ca43
Kirill Menke <ny70konu@stud.informatik.uni-erlangen.de>
2021-11-01 18:45:54 +0100

    Added distance to metro stations and proper mapping of buildings

--- 0151.py
+++ 0151.py
@@ -2,21 +2,24 @@ from math import radians
 
 import numpy as np
 import pandas as pd
+import geopandas as gp
+
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
 
 from sklearn.neighbors import BallTree, KNeighborsRegressor
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.linear_model import RidgeCV
 from sklearn.ensemble import StackingRegressor, ExtraTreesRegressor
-from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.model_selection import StratifiedGroupKFold, StratifiedKFold
 from sklearn.metrics import mean_squared_log_error
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.metrics.pairwise import haversine_distances
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer, IterativeImputer
+from sklearn.decomposition import PCA
 
-from xgboost import XGBRegressor
-from catboost import CatBoostRegressor
-from lightgbm import LGBMRegressor
 
 pd.options.mode.chained_assignment = None
 seed = np.random.seed(0)
@@ -65,11 +68,28 @@ def add_features(data_df):
     bulding_locs = np.asarray(list(zip(data_df['latitude'], data_df['longitude'])))
     sub_area_locs = np.asarray(list(zip(subareas['longitude'], subareas['latitude'])))
     closest_sub_idx = np.argmin(haversine_distances(bulding_locs, sub_area_locs), axis=1)
-    data_df["sub_area"] = subareas['sub_area'].iloc[closest_sub_idx].values
-    data_df["sub_area"] = LabelEncoder().fit_transform(data_df.sub_area)
+    data_df["sub_area_"] = subareas['sub_area'].iloc[closest_sub_idx].values
+    data_df["sub_area_"] = LabelEncoder().fit_transform(data_df['sub_area_'])
+
+    # Mapping each building to its sub_area
+    geo_df = gp.GeoDataFrame(data_df, geometry=gp.points_from_xy(data_df.longitude, data_df.latitude))
+    geo_df.crs = "EPSG:4326"
+    data_df = gp.sjoin(sub_areas_df, geo_df, how='right', predicate='contains')
+    data_df = data_df.drop(["DISTRICT", "geometry", "OKATO", "OKTMO", "OKATO_AO", "index_left"], axis=1)
+    data_df["sub_area"] = LabelEncoder().fit_transform(data_df['sub_area'])
+
+    pca = PCA(n_components=1)
+    data_df["sub_area_pca"] = pca.fit_transform(data_df[["sub_area", "sub_area_"]]).squeeze()
+    # data_df = data_df.drop(["sub_area", "sub_area_"], axis=1)
+
 
     # Add distance from city center
     data_df['center_distance'] = haversine_array(data_df['latitude'], data_df['longitude'])
+
+    # Add closest metro_station
+    metro_locs = np.asarray(list(zip(metro_stations['latitude'], metro_stations['longitude'])))
+    closest_metro_dist = np.min(haversine_distances(bulding_locs, metro_locs), axis=1)
+    data_df['closest_metro'] = closest_metro_dist
 
     # Add average price in the neighborhood
     tree = BallTree(data_df[["latitude", "longitude"]])
@@ -77,16 +97,25 @@ def add_features(data_df):
 
     # Take log of area
     data_df['area_total'] = np.log(data_df['area_total'])
-    mean_sqm_price_of_cluster = []
-    for rows in ind:
-        mean_sqm_price_of_cluster.append(np.nanmean(data_df.price.iloc[rows]/data_df.area_total.iloc[rows]))
+    mean_sqm_price = []
+    mean_subarea_price = []
 
-    data_df["mean_sqm_price_of_cluster"] = mean_sqm_price_of_cluster
+    for rows in ind:
+        mean_sqm_price.append(np.nanmean(data_df['price'].iloc[rows] / data_df['area_total'].iloc[rows]))
+        mean_subarea_price.append(np.nanmean(data_df['price'].iloc[rows]))
+
+
+    data_df["mean_sqm_price"] = mean_sqm_price
+    # data_df["mean_subarea_price"] = mean_subarea_price
+
+    return data_df
+
+
 
 
 def get_model():
 
-    model_lgbm = LGBMRegressor(max_depth=6, n_estimators=1200, learning_rate=0.1, n_jobs=THREADS)
+    model_lgbm = LGBMRegressor(max_depth=6, n_estimators=1200, learning_rate=0.1) # , n_jobs=THREADS
     model_xgb = XGBRegressor(n_estimators=1500, max_depth=6, learning_rate=0.1)
     model_cat = CatBoostRegressor(iterations=1000, depth=7, learning_rate=0.1, silent=True)
     model_extra = ExtraTreesRegressor(n_estimators=1000)
@@ -108,7 +137,7 @@ def get_model():
     model_stacking = StackingRegressor(estimators=base_learners, final_estimator=final_model, cv=5, n_jobs=THREADS)
     trans_stacking = TransformedTargetRegressor(regressor=model_stacking, func=np.log1p, inverse_func=np.expm1)
 
-    return trans_stacking
+    return trans_lgbm
 
 
 
@@ -120,11 +149,12 @@ if __name__ == "__main__":
     apartments_test = pd.read_csv("data/apartments_test.csv")
     buildings_test = pd.read_csv("data/buildings_test.csv")
     subareas = pd.read_csv("data/sberbank_sub_areas.csv")
+    metro_stations = pd.read_csv("data/metro_stations.csv")
+    sub_areas_df = gp.read_file('./data/mo_kag_SRHM.shp')
 
     # Merge Tables: Apartments and Buildings
     train_df = apartments_train.merge(buildings_train, left_on='building_id', right_on='id', suffixes=('', '_r')).sort_values('id').set_index('id')
     test_df = apartments_test.merge(buildings_test, left_on='building_id', right_on='id', suffixes=('', '_r')).sort_values('id').set_index('id')
-
 
     # Merge train and test data
     data_df = pd.concat([train_df, test_df])
@@ -133,14 +163,12 @@ if __name__ == "__main__":
     preprocess(data_df)
     
     # Add new features
-    add_features(data_df)
+    data_df = add_features(data_df)
 
     data_df = data_df.drop(["address", "street", "windows_court", "windows_street", "elevator_service", "elevator_passenger", "garbage_chute", 
-        "layout", "parking", "heating", "elevator_without", "material", "phones", "district", "building_id", "id_r"], axis = 1)
-    # "seller", "bathrooms_shared", "bathrooms_private", "balconies", "loggias"
+        "layout", "parking", "heating", "elevator_without", "district", "phones", "building_id", "id_r", "material"], axis = 1)
 
-
-    categorical_features = ["condition", "seller", "new", "street_and_address", "sub_area"]
+    categorical_features = ["condition", "seller", "street_and_address", "new"] # , "sub_area", "constructed"
     num_features = list(data_df.drop(categorical_features + ["price"], axis=1).columns)
 
     # Adding missing values as separate features
@@ -166,25 +194,25 @@ if __name__ == "__main__":
 
 
     # Perform cross-validation and prediction on test set
-    # errors = []
-    # preds = []
+    errors = []
+    preds = []
 
-    # cv = StratifiedGroupKFold()
+    cv = StratifiedKFold(shuffle=True)
     model = get_model()
 
-    # for train_idx, val_idx in cv.split(X_train, np.log(y_train).round(), groups=train_df['building_id']):
+    for train_idx, val_idx in cv.split(X_train, np.log(y_train).round()): # groups=train_df['building_id']
 
-    #     X_train_, y_train_ = X_train.iloc[train_idx], y_train.iloc[train_idx]
-    #     X_val, y_val = X_train.iloc[val_idx], y_train.iloc[val_idx]
+        X_train_, y_train_ = X_train.iloc[train_idx], y_train.iloc[train_idx]
+        X_val, y_val = X_train.iloc[val_idx], y_train.iloc[val_idx]
 
-    #     model.fit(X_train_, y_train_)
-    #     pred = model.predict(X_val)
-    #     errors.append(rmsle(y_val, pred))
+        model.fit(X_train_, y_train_)
+        pred = model.predict(X_val)
+        errors.append(rmsle(y_val, pred))
 
-    #     preds.append(model.predict(X_test))
+        preds.append(model.predict(X_test))
 
-    #     print("Error: ", errors[-1])
-    # print("Mean error: ", np.mean(errors))
+        print("Error: ", errors[-1])
+    print("Mean error: ", np.mean(errors))
 
 
     # y_pred = np.mean(preds, axis = 0)
@@ -192,20 +220,5 @@ if __name__ == "__main__":
     result = np.column_stack((X_test.index.to_numpy(), y_pred))
     np.savetxt(r'./result.csv', result, fmt=['%d', '%.3f'], delimiter=',', header="id,price_prediction", comments='')
 
-
-# Base
-# Stacked: 0.18699157235002986 (0.15348), 0.18272110079641235 (0.15670), 0.16983476356751734 (0.3)
-# LGBM: 0.19123223530194514 (k=300), 0.18963043421515094 (k=150), 0.17665640688920609 (k=50)
-
-# + Removed feature merging:
-# Stacked: 0.1866903855946278
-# LGBM: 0.19045180972718664
-
-# + Added Extra Tree Regressor
-# Stacked: 0.18637551655784695 (0.15286)
-
-# + Added IterativeImputer
-# Stacked: 0.18334376269442343 (0.15833)
-# LGBM: 0.18741017354408454
-
-# + Added IterativeImputer and nan values
+# Current model
+# LGBM: 0.12645358031498863 (0.15112)
